b91fe39fe5c24c915e519fc4d310cd93f3d7f026
Flavio Oria <flavio.oriap@gmail.com>
2024-10-08 08:40:59 +0200

    :tada: Init project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import "@/app/ui/global.css";
+import { lato } from "./ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+      <body className={`${lato.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Lato, Lusitana } from "next/font/google";
+
+// Exportando nuestras fuentes de google
+export const lato = Lato({ subsets: ["latin"], weight: ["300"] });
+
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
